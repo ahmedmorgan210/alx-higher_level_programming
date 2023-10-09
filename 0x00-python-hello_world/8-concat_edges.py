901d93d22bac942d69482c606b9092c18f8ab87d
root <root@Ahmed-M>
2023-10-09 14:45:33 +0300

    update

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:67] + " " + str[-22:-18] + " " + str[:6]
+print(str)
+
+#object-oriented programming with Python
