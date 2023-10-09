9a05633312bc8bfb388e139c98f3e325899f1bf7
root <root@Ahmed-M>
2023-10-09 13:49:30 +0300

    update

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str = "Holberton School"
-#print(f"{str}{str}{str} \n{str[:9]}\n")
 print(3 * str)
 print(str[:9])
 # we can use multiplicatin to iterate printing srting instead of using loop
