9c342a48331022815cff5b4b7956c43d5eeb9788
root <root@Ahmed-M>
2023-11-01 07:33:45 +0200

    check the char is lower or not using ord()

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-# 7-islower.py
-
-
 def islower(c):
-    """Check for lowercase characters."""
-    if ord(c) >= 97 and ord(c) <= 122:
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
         return True
     else:
         return False
