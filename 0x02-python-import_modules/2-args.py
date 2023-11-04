d357084b50b1d9a7deea88dc693d97d8d1c72907
root <root@Ahmed-M>
2023-11-04 22:46:11 +0200

    print the program's list of arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,14 +6,14 @@ if __name__ == "__main__":
     argc = len(sys.argv)
 
     if argc == 1:
-        print("{} arguments .".format(0))
+        print("{} arguments.".format(0))
 
     elif argc == 2:
-        print("{} argument :".format(argc - 1))
+        print("{} argument:".format(argc - 1))
         print(f"{1}: {sys.argv[1]}")
 
     else:
-        print("{} arguments :".format(argc - 1))
+        print("{} arguments:".format(argc - 1))
         for i in range(1, argc):
             print(f"{i}: {sys.argv[i]}")
 
