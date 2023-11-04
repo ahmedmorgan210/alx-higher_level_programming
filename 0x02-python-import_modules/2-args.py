fb891b355d8db1f03fce36ee140852ee736701ee
root <root@Ahmed-M>
2023-11-04 22:41:27 +0200

    print the program's list of arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    argc = len(sys.argv)
+
+    if argc == 1:
+        print("{} arguments .".format(0))
+
+    elif argc == 2:
+        print("{} argument :".format(argc - 1))
+        print(f"{1}: {sys.argv[1]}")
+
+    else:
+        print("{} arguments :".format(argc - 1))
+        for i in range(1, argc):
+            print(f"{i}: {sys.argv[i]}")
+
