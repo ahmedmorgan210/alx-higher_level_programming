9fb7bbb573945918f80823446036f02ed27460ef
root <root@Ahmed-M>
2023-11-05 04:57:40 +0200

    argv infinite_add - Done

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
-
+"""
+creating a program to print the sum
+of all argment values
+"""
 if __name__ == "__main__":
     import sys
 
     argc = len(sys.argv)
 
     total = 0
+"""
+a loop to iterate on every single argv
+adding it to the total
 
+"""
     for i in range(1, argc):
         total += int(sys.argv[i])
 
