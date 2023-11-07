de9ae8a70a0cab0c93575b2c0bf408d9b42099a4
root <root@Ahmed-M>
2023-11-07 06:32:37 +0200

    Byte-Code magic calculation - Done

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-
-import dis
-
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
@@ -9,10 +6,5 @@ def magic_calculation(a, b):
         for i in range(4, 6):
             c = add(c, i)
             return(c)
-
     else:
         return sub(a, b)
-
-
-
-dis.dis(magic_calculation)
