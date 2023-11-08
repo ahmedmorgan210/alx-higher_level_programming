17fadb3c6c9927cbbe5ab30f792b6ec4fbb2f62b
root <root@Ahmed-M>
2023-11-08 05:56:46 +0200

    the python calc

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    import sys
+    #import calculator_1 as calc
+    from calculator_1 import add, sub, mul, div
+    #from calc import add, sub, mul, div
+
+    argc = len(sys.argv)
+    result = 0
+
+    if argc != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    if sys.argv[2] == '+':
+        result = add(int(sys.argv[1]), int(sys.argv[3]))
+        print(f"{sys.argv[1]} + {sys.argv[3]} = {result}")
+        exit(0)
+
+    elif sys.argv[2] == '-':
+        result = sub(int(sys.argv[1]), int(sys.argv[3]))
+        print(f"{sys.argv[1]} - {sys.argv[3]} = {result}")
+        exit(0)
+
+    elif sys.argv[2] == '*':
+        result = mul(int(sys.argv[1]), int(sys.argv[3]))
+        print(f"{sys.argv[1]} * {sys.argv[3]} = {result}")
+        exit(0)
+
+    elif sys.argv[2] == '/':
+        result = div(int(sys.argv[1]), int(sys.argv[3]))
+        print(f"{sys.argv[1]} / {sys.argv[3]} = {result}")
+        exit(0)
+
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+
