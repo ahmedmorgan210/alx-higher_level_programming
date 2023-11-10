725d9c7e6c9832ac69687885b4bb646588e7a31f
root <root@Ahmed-M>
2023-11-10 14:07:44 +0200

    1-element_at.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    """
+    a function that retrieves an element from a list\
+            If idx is negative, the function should return None
+            If idx is out of range (> of number of element in my_list),
+                the function should return None
+            You are not allowed to import any module
+            You are not allowed to use try/except
+
+    """
+    if idx < 0 or idx > len(my_list):
+        return None
+    else:
+        return my_list[idx]
