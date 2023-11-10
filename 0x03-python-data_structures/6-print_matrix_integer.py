270272eb8f69e384418fc798e7840df87c806d0e
root <root@Ahmed-M>
2023-11-10 22:09:45 +0200

    6-print_matrix_integer.py - handle the empty matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -8,14 +8,12 @@ def print_matrix_integer(matrix=[[]]):
             You are not allowed to cast integers into strings
             You have to use str.format() to print integers
     """
-    if len(matrix) <= 0:
-        exit(-1)
-    else:
-
-        for i in range(len(matrix)):
-            for j in range(len(matrix[i])):
-                if j < len(matrix[i]) - 1:
-                    print("{:d}".format(matrix[i][j]), end=' ')
-
-                else:
-                    print("{:d}".format(matrix[i][j]))
+    if len(matrix) <= 0 or len(matrix[0]) <= 0:
+        print("")
+        return
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if j < len(matrix[i]) - 1:
+                print("{:d}".format(matrix[i][j]), end=' ')
+            else:
+                print("{:d}".format(matrix[i][j]))
