945ce036f08b8f528ac7ee703c8910c327cdd7a4
root <root@Ahmed-M>
2023-11-10 14:13:13 +0200

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -10,7 +10,7 @@ def element_at(my_list, idx):
             You are not allowed to use try/except
 
     """
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx > len(my_list) - 1:
         return None
     else:
         return my_list[idx]
