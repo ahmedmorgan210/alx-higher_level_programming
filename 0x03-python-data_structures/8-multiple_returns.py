db5dfc7f8826078ae7efbe0dee63cd480fa3e11d
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-11-11 00:48:26 -0800

    try to handle the empty string in task 08

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
+
 def multiple_returns(sentence):
     sen_len = len(sentence)
-
-    if sen_len == 0:
+    if sentence is None:
+        return None
+    #if sen_len == 0 and sentence[0] is None:
         sentence[0] = None
 
     return (len(sentence), sentence[0])
