5cb1eac8b00277db42597f8a79c25bf772bf9b1d
root <root@Ahmed-M>
2023-11-12 07:44:54 +0200

    11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -7,8 +7,8 @@ def delete_at(my_list=[], idx=0):
 
     new_list = my_list.copy()
 
-    if idx <= 0 or idx >= len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return my_list
-    else:
+    elif idx >= 0 and len(my_list) != 0:
         del new_list[idx]
         return new_list
