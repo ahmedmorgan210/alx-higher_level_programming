73845c0f6dd61d25ea9b3afd5874de06d64bb3a0
root <root@Ahmed-M>
2023-11-12 04:33:35 +0200

    8-multiple_returns.py - pycodestyle correction

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,9 +4,6 @@
 def multiple_returns(sentence):
     sen_len = len(sentence)
     if sen_len == 0:
-        #sentence[0] = None
         return (sen_len, None)
-    #if sen_len == 0 and sentence[0] is None:
-        #sentence[0] = None
 
     return (sen_len, sentence[0])
