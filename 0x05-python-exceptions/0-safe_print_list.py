307cd10156ac7fc7bcf49289039d25cac66f3f4e
root <root@Ahmed-M>
2023-11-21 03:27:58 +0200

    0-safe_print_list.py -> fix pycodestyle

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,8 @@
 Write a function that prints x elements of a list.\
         Prototype: def safe_print_list(my_list=[], x=0):
                 my_list can contain any type (integer, string, etc.)
-                All elements must be printed on the same line followed by a new line.
+                All elements must be printed on the same line
+                followed by a new line.
                 x represents the number of elements to print
                 x can be bigger than the length of my_list
                 Returns the real number of elements printed
@@ -13,6 +14,7 @@ Write a function that prints x elements of a list.\
                 You are not allowed to use len()
 
 """
+
 
 def safe_print_list(my_list=[], x=0):
 
@@ -32,6 +34,4 @@ def safe_print_list(my_list=[], x=0):
         return printed_items
 
     except IndexError:
-        print ("Index out of range")
-
-
+        print("Index out of range")
