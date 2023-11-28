84b0593a3f44d7fcc1d693cfd6638f5d0caeeaa4
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-11-27 19:39:06 -0800

    1-rectangle.py - check

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -54,17 +54,16 @@ class Rectangle:
 
         self.__width = value
 
-
     @property
     def height(self):
-       """
+        """
         The height of the rectangle.
 
         Returns:
             int: The height of the rectangle.
 
        """
-       return self.__height
+        return self.__height
 
     @height.setter
     def height(self, value):
@@ -84,4 +83,4 @@ class Rectangle:
         elif value < 0:
             raise ValueError("height must be >= 0")
 
-        self.height = value
+        self.__height = value
