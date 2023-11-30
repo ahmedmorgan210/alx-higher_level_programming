5a4faa9162844288f41320cb2d061dd5912331cc
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-11-29 18:55:30 -0800

    change the width and height in __init__ from private to public

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -19,8 +19,8 @@ class Rectangle:
             height (int): the Rectangle height.
 
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
