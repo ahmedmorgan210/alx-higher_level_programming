0fce44aff5cf38468934d2718d731290ce97d63f
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-12-28 05:47:05 +0000

    4-print_hexa.py - Done

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -4,7 +4,7 @@
 Write a program that prints all numbers from 0 to 98
         in decimal and in hexadecimal\
         You can only use one print function with string format
-        You can only use one loop in your code
+        You can only use one loop  in your code
         You are not allowed to store numbers or strings in a variable
         You are not allowed to import any module
 
