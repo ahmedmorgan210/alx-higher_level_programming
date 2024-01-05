00a2b770986eadea37b08f14abd7c6bdddd24f3a
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-01-05 17:25:20 +0000

    100-print_tebahpla.py - Done

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 
-# for letter in range(ord('z'), ord('a')-1, -1):
-#     print("{}{}".format(chr(letter), chr(letter - 32)), end="")
+"""
+program that prints the ASCII alphabet, in reverse order,\
+        alternating lowercase and uppercase
+        (z in lowercase and Y in uppercase)
+        - You can only use one print function with string format
+        - You can only use one loop in your code
+        - You are not allowed to store characters in a variable
+        - You are not allowed to import any module
+"""
 
 for letter in range(ord('z'), ord('a')-1, -1):
 
