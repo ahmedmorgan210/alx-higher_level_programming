08e5488d8210b6aeacc55ee6f643d0bd3a9b8522
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-01-21 21:13:35 +0000

    2-safe_print_list_integers.py - try to handle the case of x out of range

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for _ in my_list:
         len_list += 1
     if x > len_list:
-        raise IndexError ("Index Error: x is greater than the len of list")
+        raise IndexError("Index Error: x is greater than the len of list")
     for y in my_list[:x]:
         try:
             if isinstance(y, int):
