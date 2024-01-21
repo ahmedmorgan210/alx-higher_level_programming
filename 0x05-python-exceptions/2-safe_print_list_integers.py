a01ed97073906e6e1f93c30c0af90c36eb79180d
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-01-21 22:17:12 +0000

    2-safe_print_list_integers.py - try to handle the case of x out of range - Done

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,9 +5,9 @@ def safe_print_list_integers(my_list=[], x=0):
     printed_num = 0
     for i in range(x):
         try:
-            y = my_list[i]
-            if isinstance(y, int):
-                print("{:d}".format(y), end='')
+            #y = my_list[i]
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end='')
                 printed_num += 1
         except TypeError:
             continue
