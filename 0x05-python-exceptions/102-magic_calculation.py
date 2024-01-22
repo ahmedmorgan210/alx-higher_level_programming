08c35702176d91eb82a26a2e014f9692e09b7b46
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-01-22 22:07:24 +0000

    102-magic_calculation.py - Bytecode

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+    try:
+        for i in range(1, 4):
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result += (a ** b) / i
+    except Exception:
+        result = a + b
+    return result
