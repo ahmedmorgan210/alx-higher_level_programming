85058414d7b49df6182b76350e4b1fa7223c85f0
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-01-30 16:49:25 +0000

    re 2

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -6,6 +6,8 @@ def print_square(size):
     """prin the square func"""
     if type(size) is not int:
         raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
     for r in range(0, size):
         for c in range(0, size):
             if c != (size - 1):
