8d013f94600004cb1f33a33bf4b261d4653a647d
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-01-30 16:34:27 +0000

    03 -

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,5 +3,10 @@
 
 
 def say_my_name(first_name, last_name=""):
-    """say my name function"""
-    pass
+    """ Doc """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+
+    print("Fail")
