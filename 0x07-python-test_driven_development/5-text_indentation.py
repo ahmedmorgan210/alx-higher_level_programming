821d7c15e18fbd2fb83a363803ef60e7a4777a3e
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-01-30 17:24:04 +0000

    05

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -2,6 +2,19 @@
 """text indentation module"""
 
 
-def text_indentation(text):
-	"""text indentation func"""
-	pass
+def text_indentation(text=""):
+    """text indentation func"""
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    after_new_line = False
+    for c in text:
+        if after_new_line:
+            if c == " ":
+                continue
+            after_new_line = False
+        if c == '.' or c == '?' or c == ':':
+            print(c)
+            print("")
+            after_new_line = True
+        else:
+            print(c, end="")
