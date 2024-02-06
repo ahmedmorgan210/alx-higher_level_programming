cae248856926262667601daf8ca1ec783c58ccef
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-02-06 01:41:10 +0000

    re 04 -- 02

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,10 +4,10 @@
 
 def inherits_from(obj, a_class):
     """check subclass"""
-    # if (type(obj) is a_class):
-    #     return False
-    if isinstance(obj, a_class):
-        return True
-    elif type(obj) is a_class:
+    if (type(obj) is a_class):
         return False
+    elif isinstance(obj, a_class):
+        return True
+    # elif type(obj) is a_class:
+    #     return False
 
