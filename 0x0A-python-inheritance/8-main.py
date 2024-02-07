ad6ea4a6383a83340d660d1e8b14789a38c0d043
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-02-07 12:41:41 +0000

    update the 08

--- 0x0A-python-inheritance/8-main.py
+++ 0x0A-python-inheritance/8-main.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
 Rectangle = __import__('8-rectangle').Rectangle
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+r = Rectangle(3, 5)
+print(r)
+print(dir(r))
 
 try:
-    r = Rectangle(3, 5)
-    print(r.height)
+    print("Rectangle: {} - {}".format(r.width, r.height))
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    r2 = Rectangle(4, True)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+print("Rectangle:", Rectangle)
+print("BaseGeometry:", BaseGeometry)
+print("Is Rectangle a subclass of BaseGeometry?", issubclass(Rectangle, BaseGeometry))
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -9,22 +9,24 @@ class Rectangle(BaseGeometry):
         """init func"""
         # self.height = self.integer_validator("height", height)
         # self._width = width
-
-        if self.integer_validator("width", width):
+        if self.integer_validator("width", width) and\
+                self.integer_validator("height", height):
             self._width = width
-
-        if self.integer_validator("height", height):
             self.height = height
+
+        # if self.integer_validator("width", width):
+        #     self._width = width
+
+        # if self.integer_validator("height", height):
+        #     self.height = height
         # self._width = self.integer_validator("width", width)
         # self.height = self.integer_validator("height", height)
         # self.height = height
 
         # self.height = height
         # self.integer_validator("height", height)
-
-
     # @property
     # def get_height(self):
     #     """getting height"""
     #     return (self.height)
-print(dir(Rectangle))
+# print(dir(Rectangle))
