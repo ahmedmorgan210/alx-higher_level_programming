d83958834e7ba215fbd074c1ad9e8c0bd7095816
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-02-07 13:27:29 +0000

    08 - fixed

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -7,27 +7,9 @@ class Rectangle(BaseGeometry):
     """rectangle from base"""
     def __init__(self, width, height):
         """init func"""
-        # self.height = self.integer_validator("height", height)
-        # self._width = width
-        if self.integer_validator("height", height)or self.integer_validator("width", width):  
-        # if self.integer_validator("width", width) and\
-        #         self.integer_validator("height", height):
+
+        if self.integer_validator("height", height) or\
+                self.integer_validator("width", width):
+
             self._width = width
             self.height = height
-
-        # if self.integer_validator("width", width):
-        #     self._width = width
-
-        # if self.integer_validator("height", height):
-        #     self.height = height
-        # self._width = self.integer_validator("width", width)
-        # self.height = self.integer_validator("height", height)
-        # self.height = height
-
-        # self.height = height
-        # self.integer_validator("height", height)
-    # @property
-    # def get_height(self):
-    #     """getting height"""
-    #     return (self.height)
-# print(dir(Rectangle))
