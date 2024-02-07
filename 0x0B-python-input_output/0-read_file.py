84dfa9ee19128d9ce32408b7b84fb221236d0ff2
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-02-07 01:00:10 +0000

    0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    Prototype: def read_file(filename=""):
+    You must use the with statement
+    You don’t need to manage file permission or file doesn't exist exceptions.
+    You are not allowed to import any module
+"""
+
+
+def read_file(filename=""):
+    """raed a file"""
+    with open(filename, 'r', encoding="utf-8") as f:
+        reading_file = f.read()
+        print
