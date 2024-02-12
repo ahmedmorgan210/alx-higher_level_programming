096dc2327cef783aed7a1740507d11bdb1a0031c
Ahmed Hussein Morgan <39451996+ahmedmorgan210@users.noreply.github.com>
2024-02-12 16:45:48 +0000

    update the file

--- 0x0C-python-almost_a_circle/models/rectangle-another-way - t 02.py
+++ 0x0C-python-almost_a_circle/models/rectangle-another-way - t 02.py
@@ -18,7 +18,7 @@ class Base:
             self.id = self.__nb_objects
 class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
-        super().__init__(self, id)
+        super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
