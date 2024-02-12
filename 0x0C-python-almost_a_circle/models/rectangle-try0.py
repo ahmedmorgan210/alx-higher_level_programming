062d86d047b78204ce40eea015cc1b1646b5b779
Ahmed Hussein Morgan <39451996+ahmedmorgan210@users.noreply.github.com>
2024-02-12 16:29:29 +0000

    task02

---
+++ 0x0C-python-almost_a_circle/models/rectangle-try0.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+""" the base class"""
+
+
+class Base:
+    """the base of all future classes"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """manage id attribute in all your future classes\
+            and to avoid duplicating the same code
+        """
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = self.__nb_objects
+class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(self, id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
