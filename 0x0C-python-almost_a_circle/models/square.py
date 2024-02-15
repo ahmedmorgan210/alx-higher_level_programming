979823570bd08ca9300bd1e8e14d1d00a2a44360
Ahmed Hussein Morgan <39451996+ahmedmorgan210@users.noreply.github.com>
2024-02-15 13:44:29 +0000

    remove the printing type check

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,7 +7,6 @@ class Square(Rectangle):
     """the square chape class"""
     def __init__(self, size, x=0, y=0, id=None):
         """initializing thw square"""
-        print(f"Size type: {type(size)}")
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
