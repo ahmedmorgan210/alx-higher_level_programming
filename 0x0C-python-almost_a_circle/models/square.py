702489c548b3bab9d0b1480a294f29aa43b8cf66
Ahmed Hussein Morgan <39451996+ahmedmorgan210@users.noreply.github.com>
2024-02-15 13:41:59 +0000

    add the __doc__

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,11 +4,14 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """the square chape class"""
     def __init__(self, size, x=0, y=0, id=None):
+        """initializing thw square"""
         print(f"Size type: {type(size)}")
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """overriding the __str__ method of the reqtangle parent class"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
         # return f"[Square] " + \
         #     f"({self.id}) {self.x}/{self.y} " + \
