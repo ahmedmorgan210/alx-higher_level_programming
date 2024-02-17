de497152bb67720b072edeeffe944044f05dfd12
Ahmed Hussein Morgan <39451996+ahmedmorgan210@users.noreply.github.com>
2024-02-17 03:10:23 +0000

    tas 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -34,12 +34,12 @@ class Square(Rectangle):
                 self.id = args[0]
             if len(args) == 2:
                 self.size = args[1]
-            # if len(args) == 3:
-            #     self.__height = args[2]
             if len(args) == 3:
-                self.x = args[2]
+                self.size = args[2]
             if len(args) == 4:
-                self.y = args[3]
+                self.x = args[3]
+            if len(args) == 5:
+                self.y = args[4]
 
         if args:
             return
