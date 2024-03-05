aa06cbf1bfb5f12a5d88dd09795b642c9407c979
Ahmed Hussein Morgan <39451996+ahmedmorgan210@users.noreply.github.com>
2024-03-05 08:17:08 +0000

    update square

--- 0x0C-python-almost_a_circle/models/square-try.py
+++ 0x0C-python-almost_a_circle/models/square-try.py
@@ -8,24 +8,42 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """initializing thw square"""
         super().__init__(size, size, x, y, id)
-        # super().__width = size
-        # super().__height = size
+        # self.size = size
 
     @property
-    def size_getter(self):
-        return self.size
+    def size(self):
+        return self.width
 
-    @size_getter.setter
+    @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
-        self.size = value
+        self.width = value
+        self.height = value
 
     def __str__(self):
         """overriding the __str__ method of the reqtangle parent class"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
-        # return f"[Square] " + \
-        #     f"({self.id}) {self.x}/{self.y} " + \
-        #     f"- {self.size}"
+
+    def update(self, *args, **kwargs):
+        """specify the arguments"""
+        if len(args) > 0:
+            if len(args) == 1:
+                self.size = args[0]
+            if len(args) == 2:
+                self.size = args[1]
+            if len(args) == 3:
+                self.x = args[2]
+            if len(args) == 4:
+                self.y = args[3]
+            if len(args) == 5:
+                self.id = args[4]
+
+        if args:
+            return
+
+        for key, value in kwargs.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
