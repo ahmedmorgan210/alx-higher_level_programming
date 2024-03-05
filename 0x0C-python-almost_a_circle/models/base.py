0abbe51fbd137f2e2828a55088be0c69aae7cde8
Ahmed Hussein Morgan <39451996+ahmedmorgan210@users.noreply.github.com>
2024-03-05 14:30:39 +0000

    save to file 1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,3 +26,18 @@ class Base:
 
         str_rep = json.dumps(list_dictionaries)
         return str_rep
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file"""
+
+        if list_objs is None:
+            list_objs = []
+
+        class_name = cls.__name__
+
+        file_name = f"{class_name}.json"
+        json_string = cls.to_json_string(list_objs)
+
+        with open(file_name, 'w') as file:
+            file.write(json_string)
