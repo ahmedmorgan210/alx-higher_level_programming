ff7a0bb9aa00d951da571ca88ddecfc7ee3fff73
Ahmed Hussein Morgan <39451996+ahmedmorgan210@users.noreply.github.com>
2024-04-09 13:12:07 +0000

    update

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const size = Number(process.argv);
+
+if (size) {
+  for (let x = 0; x < size; x++) {
+    for (let y = 0; y < ; y++) {
+      console.log('X');
+    }
+  }
+} else {
+  console.log('Missing size');
+}
