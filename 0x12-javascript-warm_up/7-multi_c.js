2622ce415a0a0999912c865bdb5bffb8d1fac028
Ahmed Hussein Morgan <39451996+ahmedmorgan210@users.noreply.github.com>
2024-04-09 11:50:15 +0000

    task 07

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,7 +5,7 @@ const number = Number(process.argv[2]);
 if (number) {
   for (let x = 0; x < number; x++) {
     console.log('C is fun');
-  } 
+  }
 } else {
-  console.log('Missing number of occurrences')
+  console.log('Missing number of occurrences');
 }
