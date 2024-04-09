80d79d568ffc1737eccea63c20e61ae8f636a71d
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-04-09 04:44:25 +0200

    task 03

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+// const args = process.argv;
+
+if (process.argv[2]) {
+  console.log(process.argv[2]);
+} else {
+  console.log('No argument');
+}
+// let numArgs = 0;
+
+// for (let x in args) {
+//   numArgs += 1;
+// }
+
+// if (numArgs === 2) {
+//   console.log('No argument');
+// } else {
+//   console.log(args[3]);
+// }
