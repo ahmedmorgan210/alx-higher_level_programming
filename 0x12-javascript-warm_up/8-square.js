cf2b5a9094d6dac6063ed69d9810d4523043b372
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-04-09 23:45:24 +0200

    task 08

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
 
-const size = Number(process.argv);
+const size = Number(process.argv[2]);
 
 if (size) {
   for (let x = 0; x < size; x++) {
-    for (let y = 0; y < ; y++) {
-      console.log('X');
+    let row = '';
+    for (let y = 0; y < size; y++) {
+      row += 'X';
     }
+    console.log(row);
   }
 } else {
   console.log('Missing size');
