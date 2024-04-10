567df79215d6dd72bf5e2fb49e0ba10359ba067b
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-04-10 14:15:49 +0200

    add comments to explain the code

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,10 +1,13 @@
 #!/usr/bin/node
+
+// replace the value 12 with 89
 const myObject = {
   type: 'object',
   value: 12
 };
 console.log(myObject);
 
+// update the needed key (value) of the dictionary
 myObject.value = 89;
 
 console.log(myObject);
