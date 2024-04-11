a3392bfb66b68c1fb88d87e8de843b56a82fcb0e
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-04-11 15:26:46 +0200

    task 01

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
+
 class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
 }
 
 // In Node.js, to make a class or any other module available to other files, you need to export it using module.exports.
