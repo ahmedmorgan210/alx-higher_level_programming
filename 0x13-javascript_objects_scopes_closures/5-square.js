705fb002b9e75d4c191deb2cd96b9207f1c75dc2
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-04-11 18:39:11 +0200

    task 05

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+}
+
+// In Node.js, to make a class or any other module available to other files, you need to export it using module.exports.
+
+module.exports = Square;
