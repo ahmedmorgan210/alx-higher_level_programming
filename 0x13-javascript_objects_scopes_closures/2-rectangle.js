70eca7c83480f1b89d70d7ac24dcdb6cd5f4e1b8
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-04-11 16:08:02 +0200

    update task 02

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,9 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
+    if (!w || !h) {
+      // empty instance
+    } else if (w <= 0 || h <= 0) {
       // this =
     } else {
       this.width = w;
