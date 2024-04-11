9bd284a36ce8048c2cb155798d18dd6fbfec738d
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-04-11 21:38:55 +0200

    task 06

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+const Ex = require('./5-square');
+
+class Square extends Ex{
+
+  // constructor (size) {
+  //   super(size);
+  // }
+
+  charPrint(c) {
+    if (!c) {
+        this.print();
+    } else {
+        for (let x = 0; x < this.height; x++) {
+            let row = '';
+            for (let y = 0; y < this.width; y++) {
+              row += c;
+            }
+            console.log(row);
+        }
+    }
+  }
+}
+
+// In Node.js, to make a class or any other module available to other files, you need to export it using module.exports.
+
+module.exports = Square;
