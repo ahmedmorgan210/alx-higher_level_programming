d855651d12962e79f05e4e182cc50e4163e433f3
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-04-13 01:34:50 +0200

    try 000

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -20,7 +20,7 @@ import MySQLdb
 
 def main ():
 	"""create a connection and retreive the states"""
-	db_conn = MySQLdb.connect(user='root', passwd='root')
+	db_conn = MySQLdb.connect(user='root', passwd='root', db='hbtn_0e_0_usa')
 	cur = db_conn.cursor()
 	cur.execute("SELECT * FROM states ORDER BY id ASC;")
 	data = cur.fetchall()
