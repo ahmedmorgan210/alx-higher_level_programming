4bd4231142cf2afa88d939cba160713dd906aa8a
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-04-13 16:09:48 +0200

    task 04

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Write a script that lists all cities from the database hbtn_0e_4_usa
+
+    Your script should take 3 arguments: mysql username,\
+        mysql password and database name
+    You must use the module MySQLdb (import MySQLdb)
+    Your script should connect to a MySQL server\
+        running on localhost at port 3306
+    Results must be sorted in ascending order by cities.id
+    You can use only execute() once
+    Results must be displayed as they are in the example below
+    Your code should not be executed when imported
+
+"""
+
+import sys
+import MySQLdb
+"""import the mysql DB-API"""
+
+
+def main(*args, **kwargs):
+    """create a connection and retreive the states"""
+    db_conn = MySQLdb.connect(host="localhost",
+                              port=3306,
+                              user=sys.argv[1],
+                              passwd=sys.argv[2],
+                              db=sys.argv[3],
+                              charset="utf8")
+    cur = db_conn.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name\
+                FROM cities\
+                JOIN states ON cities.state_id = states.id\
+                ORDER BY cities.id ASC;")
+    # cur.execute("SELECT * FROM states ORDER BY id ASC;")
+    data = cur.fetchall()
+
+    for row in data:
+        print(row)
+
+    cur.close()
+    db_conn.close()
+
+
+if __name__ == "__main__":
+    main()
