a4af00555fec41e92b778aba7ebcf744e5cb4dcf
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-04-13 03:04:15 +0200

    FIX STILE - TASK 01

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,7 +6,8 @@ Write a script that lists all states with a name starting with N (upper N)
     Your script should take 3 arguments: mysql username,
         mysql password and database name (no argument validation needed)
     You must use the module MySQLdb (import MySQLdb)
-    Your script should connect to a MySQL server running on localhost at port 3306
+    Your script should connect to a MySQL server running on
+         localhost at port 3306
     Results must be sorted in ascending order by states.id
     Results must be displayed as they are in the example below
     Your code should not be executed when imported
