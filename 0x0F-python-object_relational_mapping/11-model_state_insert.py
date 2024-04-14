b511584a476f2d587e02fefe6403b5a13e00ac92
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-04-15 01:32:58 +0200

    task 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+Display the first state in states.id
+    You are not allowed to fetch all states from the database
+    before displaying the result
+"""
+
+import sys
+from sqlalchemy import create_engine, String, Integer, Column
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class States(Base):
+    """
+    Represent a state in 'states' table
+    """
+    __tablename__ = 'states'
+    id = Column("id", Integer, autoincrement=True, nullable=False,
+                primary_key=True)
+    name = Column("name", String(128), nullable=False)
+
+
+def main():
+    """
+    The main func to fetch and display all states in database
+    """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    #result = session.query(States).filter(States.name == sys.argv[4]).all()
+    new_state = States(name="Louisiana")
+    session.add(new_state)
+    session.commit()
+
+    result = session.query(States).filter(States.name == "Louisiana").first()
+
+    if not result:
+        print("Not found")
+    else:
+        # for i, state in enumerate(result, start=1):
+        #     print(f"{state.id}: {state.name}")
+
+        # The below loop also correct and display the same
+        #       result as  the above one
+
+        # for x in result:
+        #     print(f"{x.id}")
+
+        print(f"{result.id}")
+
+
+if __name__ == "__main__":
+    main()
