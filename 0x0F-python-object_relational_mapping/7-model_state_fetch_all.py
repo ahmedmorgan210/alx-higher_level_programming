806f0ff33f19d8f4f42fc1be0ad6b3c3fbe82e2f
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-04-14 17:47:56 +0200

    update

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 
 """
-Write a script that lists all State objects from the database hbtn_0e_6_usa
+    Write a script that lists all State objects from the database hbtn_0e_6_usa
 
-Your script should take 3 arguments: mysql username, mysql password and database name
-You must use the module SQLAlchemy
-You must import State and Base from model_state - from model_state import Base, State
-Your script should connect to a MySQL server running on localhost at port 3306
-Results must be sorted in ascending order by states.id
-The results must be displayed as they are in the example below
-Your code should not be executed when imported
+    Your script should take 3 arguments: mysql username, mysql password and database name
+    You must use the module SQLAlchemy
+    You must import State and Base from model_state - from model_state import Base, State
+    Your script should connect to a MySQL server running on localhost at port 3306
+    Results must be sorted in ascending order by states.id
+    The results must be displayed as they are in the example below
+    Your code should not be executed when imported
 """
 
 
@@ -21,15 +21,10 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                       .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                       pool_pre_ping=True)
-
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-""" try to document 
-"""
 class State(Base):
     """defining the states table"""
     __tablename__ = 'states'
@@ -38,17 +33,11 @@ class State(Base):
     name = Column("name", String(128), nullable=False)
 
 
-# engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-#                         .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-#                         pool_pre_ping=True)
-
-
-# Session = sessionmaker()
-# session = Session()
 result = session.query(State).order_by(State.id).all()
 
 for sort, state in enumerate(result, start=1):
     print(f"{sort}: {state.name}")
 
 if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
