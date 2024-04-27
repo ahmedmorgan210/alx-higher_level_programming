0c3354661b0749d2fd381fb0a9e91d71e9ae14a4
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-04-27 07:45:20 -0700

    try in task 01

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # desplay the reponse in case of state 200
-curl -s $1 | { read -r status; echo "$status"; if [ "$status" = "HTTP/1.1 200 OK" ]; then cat; fi; }
+curl -sL $1 | { read -r status; echo "$status"; if [ "$status" = "HTTP/1.1 200 OK" ]; then cat; fi; }
