8fb183f853f25ed8f2d934b7fddd83865cd4aea5
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-04-27 05:57:44 +0300

    update

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-"""Python script to fetch and print the 10 most recent commits from a GitHub repository"""
+"""Python script to fetch and print the 10 most\
+      recent commits from a GitHub repository"""
+
 
 import requests
 import sys
+"""The base URL for the GitHub API"""
 
-# The base URL for the GitHub API
+
 base_url = "https://api.github.com/repos"
 
 # The repository and owner names are passed as command-line arguments
