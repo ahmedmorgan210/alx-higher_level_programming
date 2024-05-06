010ebc643f5a0c3ed65db1026f635acf2ab45297
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-05-06 17:17:09 +0300

    task 13 .. 002

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -16,6 +16,6 @@ ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres 
 ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_genres.name
-HAVING COUNT(tv_shows.title) > 0
--- WHERE tv_show_genres.show_id IS NULL
+HAVING COUNT(tv_shows.title) > 0 AND tv_genres.name IS NOT NULL
+WHERE tv_show_genres.show_id  NULL
 ORDER BY number_of_shows DESC;
