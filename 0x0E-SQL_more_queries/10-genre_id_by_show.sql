441f65fc531d5068351736a9ae100c1c613a891c
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-05-06 14:25:51 +0300

    UPDATE - 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -7,5 +7,6 @@
 --     You can use only one SELECT statement
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id > 1
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC
