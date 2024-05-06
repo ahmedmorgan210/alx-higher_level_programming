2da5189acaccd3ae2dffb83c30744c1c448af539
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-05-06 16:18:42 +0300

    TASK 13 .. 0

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,18 @@
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server: download (same as 12-no_genre.sql)
+
+-- Write a script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+
+--     Each record should display: <TV Show genre> - <Number of shows linked to this genre>
+--     First column must be called genre
+--     Second column must be called number_of_shows
+--     Don’t display a genre that doesn’t have any shows linked
+--     Results must be sorted in descending order by the number of shows linked
+--     You can use only one SELECT statement
+--     The database name will be passed as an argument of the mysql command
+SELECT tv_genres.name AS "genre", COUNT(tv_genres.id) AS "number_of_shows"
+FROM tv_genres
+LEFT JOIN tv_show_genres 
+ON tv_show_genres.show_id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+-- WHERE tv_show_genres.show_id IS NULL
+ORDER BY number_of_shows DESC;
