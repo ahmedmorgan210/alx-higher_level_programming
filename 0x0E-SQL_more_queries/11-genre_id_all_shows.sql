0d008574c622a85604848e9a63610e66774182a8
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-05-06 14:55:57 +0300

    TASK 11 .. 01

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -9,6 +9,6 @@
 --     The database name will be passed as an argument of the mysql command
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-RIGHT JOIN tv_show_genres 
+LEFT JOIN tv_show_genres 
 ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
