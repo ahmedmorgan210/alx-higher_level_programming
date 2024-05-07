cdd755584e685c1ee1c8aa683f571bbe6cc50867
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-05-07 11:41:58 +0300

    task 15 .. 01

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -7,7 +7,7 @@
 --     Results must be sorted in ascending order by the show title
 --     You can use only one SELECT statement
 --     The database name will be passed as an argument of the mysql command
-SELECT tv_shows.title AS "Title"
+SELECT tv_shows.title AS "title"
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
 LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
