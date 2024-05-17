2c8eec8709586660dda05be750b4c8662508103d
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-05-18 02:09:14 +0300

    task 1 - try 01

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """display the value of the X-Request-Id variable found in the header """
 from urllib.request import urlopen
+import sys
 
 
-with urlopen('https://alx-intranet.hbtn.io/status') as response:
+with urlopen(sys.argv[1]) as response:
     html = response.getheader('X-Request-Id')
 
 print(html)
