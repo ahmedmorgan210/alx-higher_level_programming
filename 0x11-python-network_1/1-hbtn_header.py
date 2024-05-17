918e9f1a9710638d185243849ec192a60652a754
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-05-18 02:16:12 +0300

    task 1 - try 02 (documentation)

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""display the value of the X-Request-Id variable found in the header """
+""" display the value of the X-Request-Id variable found in the header """
+
 from urllib.request import urlopen
 import sys
 
