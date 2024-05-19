c51596ae0fb8fb320cac5bd71be8a073bd3c2b55
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-05-19 04:38:20 +0300

    task 2 - try 00

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""sends a POST request to the passed URL with the email as a parameter"""
+import sys
+import urllib
+import urllib.parse
+import urllib.request
+
+url = sys.argv[1]
+value = {'email': sys.argv[2]}
+
+data = urllib.parse.urlencode(value)
+data = data.encode("utf-8")
+req = urllib.request.Request(url, data)
+
+with urllib.request.urlopen(req) as response:
+    the_response = response.read()
+
+print(f"Your email is: {the_response}")
