e2c4c9a81e3c142181de9abddbc55934df3bf8b5
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-05-22 16:06:15 +0300

    task 0 - try 00

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,25 @@
 #!/usr/bin/node
 const fs = require('fs').promises;
 
-async function readFile(filePath) {
-    try {
-        const data = await fs.readFile(filePath);
-        console.log(data.toString());
-    } catch (error) {
-        console.error(error);
-    }
+async function readFile (filePath) {
+  try {
+    const data = await fs.readFile(filePath);
+    console.log(data.toString());
+  } catch (error) {
+    console.error(error);
+  }
 }
-readFile(process.argv[2])
+readFile(process.argv[2]);
+
+// try {
+//   fs.readFile(process.argv[2]).then(data => {
+//     // Once the promise resolves, convert the buffer to a string and log it
+//     console.log(data.toString());
+//   }).catch(error => {
+//     // Handle any errors that occur during file reading
+//     console.error(error);
+//   });
+// } catch (error) {
+//   // Handle any other errors that might occur
+//   console.error(error);
+// }
