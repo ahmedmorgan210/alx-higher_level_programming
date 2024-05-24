06459e673beb523403ae98098879a53a6af1b397
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-05-25 00:45:27 +0300

    task 5 - try 01 (fix style)

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,23 +3,23 @@
 const fs = require('fs').promises;
 const request = require('request');
 
-async function writingFile(urlToText, filePath){
-  options = {
-    url: urlToText,
-    json: true
-  };
+async function writingFile (urlToText, filePath) {
+  // options = {
+  //   url: urlToText,
+  //   json: true
+  // };
   try {
-    request(options, function(err, response, body){
-      if (!err && response.statusCode == 200) {
-          fs.writeFile(filePath, body);
+    request(urlToText, function (err, response, body) {
+      if (!err && response.statusCode === 200) {
+        const responseJson = JSON.parse(body);
+        fs.writeFile(filePath, responseJson);
       } else {
         console.error(err);
       }
-    })
+    });
   } catch (error) {
     console.error(error);
   }
-};
+}
 
-
-writingFile(process.argv[2], process.argv[3])
+writingFile(process.argv[2], process.argv[3]);
