105b7d44406c0ea0ecb375f493b536e6cf7cc6b5
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-05-25 10:53:35 +0300

    task 5 - try 08 (check empty file and writing failed)

---
+++ 0x14-javascript-web_scraping/5-request_store--try22.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+const fs = require('fs').promises;
+const request = require('request');
+
+async function writingFile (urlToText, filePath) {
+  try {
+    request(urlToText, { json: true }, function (err, response, body) {
+      if (err || !response || response.statusCode !== 200) {
+        console.error('Error fetching data:', err || 'Request failed');
+        return;
+      }
+
+      if (body && Object.keys(body).length !== 0) {
+        fs.writeFile(filePath, JSON.stringify(body)).then(() => {
+          console.log('Data written successfully.');
+        }).catch((writeErr) => {
+          console.error('Error writing file:', writeErr);
+        });
+      } else {
+        console.log('No data received or data is empty.');
+      }
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+if (process.argv[3]) {
+  writingFile(process.argv[2], process.argv[3]);
+} else {
+  console.log('');
+}
--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,21 +4,34 @@ const fs = require('fs').promises;
 const request = require('request');
 
 async function writingFile (urlToText, filePath) {
+  // options = {
+  //   url: urlToText,
+  //   json: true
+  // };
   try {
-    request(urlToText, { json: true }, function (err, response, body) {
-      if (err || !response || response.statusCode !== 200) {
-        console.error('Error fetching data:', err || 'Request failed');
-        return;
-      }
 
-      if (body && Object.keys(body).length !== 0) {
-        fs.writeFile(filePath, JSON.stringify(body)).then(() => {
-          console.log('Data written successfully.');
-        }).catch((writeErr) => {
-          console.error('Error writing file:', writeErr);
-        });
+    request(urlToText, { json: true }, function (err, response, body) {
+      if (!err && response.statusCode === 200) {
+        // const responseJson = JSON.parse(body);
+
+        if (!fs.writeFile(filePath, body)){
+          console.log('error happen when writing to file - writing failed')
+        };
+
+        // if (Object.keys(body).length !== 0){
+        //   fs.writeFile(filePath, body);
+        // }
+
+        
+        
+        // if (Object.keys(body).length === 0){
+        //   console.log('');
+        // } else {
+        //   fs.writeFile(filePath, body);
+        // }
+
       } else {
-        console.log('No data received or data is empty.');
+        console.error(err);
       }
     });
   } catch (error) {
@@ -26,7 +39,7 @@ async function writingFile (urlToText, filePath) {
   }
 }
 
-if (process.argv[3]) {
+if (process.argv[3]){
   writingFile(process.argv[2], process.argv[3]);
 } else {
   console.log('');
