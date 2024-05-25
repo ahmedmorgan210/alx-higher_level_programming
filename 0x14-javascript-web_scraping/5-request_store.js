0636e3ce422882bc664ad16a31d403f5cb429159
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-05-25 10:45:13 +0300

    task 5 - try 06 (check empty file)

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,31 +3,22 @@
 const fs = require('fs').promises;
 const request = require('request');
 
-async function writingFile (urlToText, filePath) {
-  // options = {
-  //   url: urlToText,
-  //   json: true
-  // };
+async function writingFile(urlToText, filePath) {
   try {
-
     request(urlToText, { json: true }, function (err, response, body) {
-      if (!err && response.statusCode === 200) {
-        // const responseJson = JSON.parse(body);
+      if (err ||!response || response.statusCode!== 200) {
+        console.error('Error fetching data:', err || 'Request failed');
+        return;
+      }
 
-        // fs.writeFile(filePath, body);
-
-        if (Object.keys(body).length != 0){
-          fs.writeFile(filePath, body);
-        }
-        
-        // if (Object.keys(body).length === 0){
-        //   console.log('');
-        // } else {
-        //   fs.writeFile(filePath, body);
-        // }
-
+      if (body && Object.keys(body).length!== 0) {
+        fs.writeFile(filePath, JSON.stringify(body)).then(() => {
+          console.log('Data written successfully.');
+        }).catch((writeErr) => {
+          console.error('Error writing file:', writeErr);
+        });
       } else {
-        console.error(err);
+        console.log('No data received or data is empty.');
       }
     });
   } catch (error) {
@@ -35,7 +26,7 @@ async function writingFile (urlToText, filePath) {
   }
 }
 
-if (process.argv[3]){
+if (process.argv[3]) {
   writingFile(process.argv[2], process.argv[3]);
 } else {
   console.log('');
