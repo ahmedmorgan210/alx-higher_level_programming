51d6eafa4db7b15aa96c8b7c2c86bf8db10d16ce
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-05-25 12:45:18 +0300

    task 5 - try 11

--- 0x14-javascript-web_scraping/5-request_store---3.js
+++ 0x14-javascript-web_scraping/5-request_store---3.js
@@ -7,13 +7,14 @@ async function writingToFile (contentUrl, filePath) {
   try {
     let data = '';
 
+    await fs.writeFile(filePath, data);
     request(contentUrl, { json: true }, function (err, res, body) {
       if (!err && res.statusCode === 200) {
         data += body;
       }
     });
 
-    await fs.writeFile(filePath, data);
+    // await fs.writeFile(filePath, data);
     console.log('Data written successfully');
   } catch (error) {
     console.error(error);
