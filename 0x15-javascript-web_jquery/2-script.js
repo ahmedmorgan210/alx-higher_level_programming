8f0bf40e74c90dd0cadbf5338065a7b731af4161
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-05-31 23:02:44 +0300

    task 02 and task 03 - try 00

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,5 @@
+$(function() {
+    $('#red_header').on('click', function() {
+        $('header').css({color:'red'})
+    });
+});
---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,6 @@
+$(function() {
+    $('#red_header').on('click', function(){
+        // add 'red' as a class name to the element 'header'
+        $('header').add('red')
+    });
+});
