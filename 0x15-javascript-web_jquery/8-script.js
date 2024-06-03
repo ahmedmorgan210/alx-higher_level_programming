2ce48ee50ac347aceb4e1c868dfeaa7f69ea1451
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-06-03 04:29:22 +0300

    task 08 - try 00

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,11 @@
+$(function(){
+    $.ajax({
+        type: "GET",
+        url: "https://swapi-api.alx-tools.com/api/films/?format=json",
+        success: function(data){
+            $.each(data.results, function(index, film){
+                $("#list_movies").append("<li>" + film.title + "</li>");
+            })
+        }
+    });
+});
