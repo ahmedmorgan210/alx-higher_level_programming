18fd6945f39e9912441be79893a00bed1aba2afd
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2025-04-07 08:05:17 +0200

    task 02

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 
-console.log(argv.length);
 if ((argv.length - 2) === 0) {
   console.log('No argument');
 } else if ((argv.length - 2) === 1) {
