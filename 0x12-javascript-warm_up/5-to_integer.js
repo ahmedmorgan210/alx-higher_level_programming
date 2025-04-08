db1006518de43ae308a0ed3bc33c4cfee3abc234
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2025-04-08 07:32:54 +0200

    task 05 - try

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// const { argv } = require('node:process');
+const argv = process.argv;
+const convertedNumber = Math.floor(Number(argv[2]));
+
+if (!convertedNumber) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${argv[2]}`);
+}
