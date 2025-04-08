8ef81de4143b57bc6665f656f2e7ce654bfef314
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2025-04-08 06:31:09 +0200

    update - task 03 - avoid using length

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 
-if ((argv.length - 2) === 0) {
+const args = process.argv.slice(2);
+
+if (argv[2] == null) {
   console.log('No argument');
 } else {
   // remove the first 2 elements passed to get the actual argumet passed
   // https://stackoverflow.com/questions/50359735/how-do-i-make-process-argv-print-only-the-arguments-and-not-the-path-or-node-co
-  const args = process.argv.slice(2);
+  // const args = process.argv.slice(2);
   console.log(args[0]);
 }
