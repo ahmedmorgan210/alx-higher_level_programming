cfc63195f5bb6269719f00e5147a1b071883025b
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2025-04-08 06:15:41 +0200

    task 03

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+
+if ((argv.length - 2) === 0) {
+  console.log('No argument');
+} else {
+  // remove the first 2 elements passed to get the actual argumet passed
+  // https://stackoverflow.com/questions/50359735/how-do-i-make-process-argv-print-only-the-arguments-and-not-the-path-or-node-co
+  const args = process.argv.slice(2);
+  console.log(args[0]);
+}
