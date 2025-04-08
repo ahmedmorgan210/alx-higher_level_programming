b8a0deda49ea6b86ad2a9f9abb25f5eec7e98574
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2025-04-08 06:35:52 +0200

    make code more readabale

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 
-const args = process.argv.slice(2);
+// const args = process.argv.slice(2);
+
+// https://nodejs.org/api/process.html#process_process_argv       # How to handle arguments passed to the script seams like argc and argv in C language
 
 if (argv[2] == null) {
   console.log('No argument');
@@ -9,5 +11,6 @@ if (argv[2] == null) {
   // remove the first 2 elements passed to get the actual argumet passed
   // https://stackoverflow.com/questions/50359735/how-do-i-make-process-argv-print-only-the-arguments-and-not-the-path-or-node-co
   // const args = process.argv.slice(2);
-  console.log(args[0]);
+//   console.log(args[0]);
+  console.log(argv[2]);
 }
