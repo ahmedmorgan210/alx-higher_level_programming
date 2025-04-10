e41ce84ac64c482a7d3daaad46b4ecd8a24731e3
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2025-04-10 07:14:34 +0200

    update - resource

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,6 +2,9 @@
 
 const { argv } = require('node:process');
 
+// Resource in the below link
+// https://www.w3schools.com/jsref/jsref_sort.asp
+
 if (argv.length < 4) {
   console.log('0');
 } else {
