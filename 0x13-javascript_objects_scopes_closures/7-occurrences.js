2598a87041412f6a0eeda79cd0d30188700876c1
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2025-04-24 22:16:56 +0200

    task 07 - try 00

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let repete = 0;
+  for (let i = 0; i < list.length; i++) {
+    // let repete = 0;
+    if (list[i] == searchElement) {
+      repete += 1;
+    }
+  }
+  return repete;
+};
