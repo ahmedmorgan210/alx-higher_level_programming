3d2501bd655e29752fb8c0bf0ee1deeab1c24481
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2025-04-24 17:48:34 +0200

    task 04 - try 02 -  fix style

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,51 +1,36 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constructor (w, h) {
-        if (w <= 0 || !w || h <= 0 || !h) {
-          return this;
-        } else {
-          this.width = w;
-          this.height = h;
-        }
-      }
-    
-    print () {
-        for (let x = 0; x < this.height; x++) {
-          let output2 = '';
-          for (let y = 0; y < this.width; y++) {
-            output2 += 'X';
-          }
-          console.log(output2);
-        }
-      }
-
-    double() {
-        this.width = this.width * 2
-        this.height = this.height * 2
-        // for (let i = 0; i < this.height * 2; i++) {
-        //     let output = '';
-        //     for (let j = 0; j < this.width * 2; j++) {
-        //         output += 'X';
-        //     }
-        //     console.log(output);
-        // }
-        
+  constructor (w, h) {
+    if (w <= 0 || !w || h <= 0 || !h) {
+      return this;
+    } else {
+      this.width = w;
+      this.height = h;
     }
+  }
 
-    rotate() {
-
-        [this.width , this.height] = [this.height, this.width];
-        // for (let i = 0; i < this.height; i++) {
-        //     let output = '';
-        //     for (let j = 0; j < this.width; j++) {
-        //       output += 'X';
-        //     }
-        //     console.log(output);
-        // }
+  print () {
+    for (let x = 0; x < this.height; x++) {
+      let output2 = '';
+      for (let y = 0; y < this.width; y++) {
+        output2 += 'X';
+      }
+      console.log(output2);
     }
+  }
 
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
 
+  rotate () {
+    // swap values of height and width using unpacking / Destructuring in JS
+    // Resource:
+    //      https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring
+    [this.width, this.height] = [this.height, this.width];
+  }
 }
 
 module.exports = Rectangle;
