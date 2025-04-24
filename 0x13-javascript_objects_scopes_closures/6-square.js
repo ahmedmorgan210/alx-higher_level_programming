d0f368fc21ab2218aa72be68d5ce338c1df93f5e
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2025-04-24 21:39:59 +0200

    task 06 - try 01

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,17 +3,18 @@
 const parentSquare = require('./5-square.js');
 
 class Square extends parentSquare {
-  charPrint (c) {
-    if (c) {
-      for (let x = 0; x < this.height; x++) {
-        let output = '';
-        for (let y = 0; y < this.width; y++) {
-          output += 'C';
-        }
-        console.log(output);
-      }
+  charPrint(c) {
+    if (!c) {
+        this.print();
+
     } else {
-      this.print();
+        for (let x = 0; x < this.height; x++) {
+            let output = '';
+            for (let y = 0; y < this.width; y++) {
+              output += 'C';
+            }
+            console.log(output);
+          }
     }
   }
 }
