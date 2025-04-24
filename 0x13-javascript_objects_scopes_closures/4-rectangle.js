ceb2d00faba4d3d87001517965630454ae93cb83
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2025-04-24 17:44:51 +0200

    task 04 - try 01

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,48 +1,51 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor (w, h) {
-    if (w <= 0 || !w || h <= 0 || !h) {
-      return this;
-    } else {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      let output = '';
-      for (let j = 0; j < this.width; j++) {
-        output += 'X';
+    constructor (w, h) {
+        if (w <= 0 || !w || h <= 0 || !h) {
+          return this;
+        } else {
+          this.width = w;
+          this.height = h;
+        }
       }
-      console.log(output);
-    }
-  }
-
-  rotate () {
-    const swappedWidth = this.height;
-    const swappedHeight = this.width;
-    for (let i = 0; i < swappedHeight; i++) {
-      let output = '';
-      for (let j = 0; j < swappedWidth; j++) {
-        output += 'X';
+    
+    print () {
+        for (let x = 0; x < this.height; x++) {
+          let output2 = '';
+          for (let y = 0; y < this.width; y++) {
+            output2 += 'X';
+          }
+          console.log(output2);
+        }
       }
-      console.log(output);
-    }
-  }
 
-  double () {
-    const dobleWidth = this.width * 2;
-    const dobleHeight = this.height * 2;
-    for (let i = 0; i < dobleHeight; i++) {
-      let output = '';
-      for (let j = 0; j < dobleWidth; j++) {
-        output += 'X';
-      }
-      console.log(output);
+    double() {
+        this.width = this.width * 2
+        this.height = this.height * 2
+        // for (let i = 0; i < this.height * 2; i++) {
+        //     let output = '';
+        //     for (let j = 0; j < this.width * 2; j++) {
+        //         output += 'X';
+        //     }
+        //     console.log(output);
+        // }
+        
     }
-  }
+
+    rotate() {
+
+        [this.width , this.height] = [this.height, this.width];
+        // for (let i = 0; i < this.height; i++) {
+        //     let output = '';
+        //     for (let j = 0; j < this.width; j++) {
+        //       output += 'X';
+        //     }
+        //     console.log(output);
+        // }
+    }
+
+
 }
 
 module.exports = Rectangle;
