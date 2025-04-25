0965a3f621ce48fb6c292538e47dc8aa52c5f76d
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2025-04-25 10:47:09 +0300

    task 100 - try 01

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,10 +2,10 @@
 
 const { list } = require('./100-data');
 
-const map = require('./100-data').list;
+const map1 = require('./100-data').list;
 
 const newList = list.map((value, index) => {
   return value * index;
 });
-
+console.log(map1)
 console.log(newList);
