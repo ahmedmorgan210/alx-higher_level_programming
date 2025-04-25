5c52edb3a3063573a4a76e1fe7e4fc3c908409de
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2025-04-25 10:08:03 +0300

    task 10 - try 01

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -2,12 +2,6 @@
 
 exports.converter = function (base) {
   return function (num) {
-    const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
-    let result = '';
-    while (num > 0) {
-      result = digits[num % base] + result;
-      num = Math.floor(num / base);
-    }
-    return result;
+    return num.toString(base);
   };
 };
